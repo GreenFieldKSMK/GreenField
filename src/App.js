d9d663e1abae7d0a3b3f3ff2c41f0951165d718b
MhmdHourani <m.hourani98@gmail.com>
2020-07-12 17:19:20 +0300

    Api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import SignUp from './components/SignUp';
+import Change from './components/ApiReact';
 
 function App() {
   return (
     <div className='App'>
       <SignUp />
+      <Change />
     </div>
   );
 }
