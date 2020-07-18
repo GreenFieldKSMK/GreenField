b133ef93988a347a62b4140c895cc0b7cee35da5
unknown <saritadh74@gmail.com>
2020-07-18 10:59:51 +0300

    finished

---
+++ src/components/about.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <div className='box1'>
+          <p className='about'>
+            Welcome dear customer, Xchange is designed to make your finaccial
+            activities easy and effortless. We deliver multiple services to our
+            beloved customers, such as the ability to deposite, withdraw, and
+            even transfer money from one account to another, so feel free to
+            join our amazing and wonderful family.
+          </p>
+          <br></br>
+          <br></br>
+          <p className='about'>Ps: &nbsp; We only allow rich people to join.</p>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default About;
